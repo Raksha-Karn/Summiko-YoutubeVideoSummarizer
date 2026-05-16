0da79f2dc569f6cccd10b3c400b52112f7d8e8e3
Raksha Karn <karnraksha41@gmail.com>
2026-05-16 20:20:33 +0545

    chore: add punctuation to transcript

--- summarizer.py
+++ summarizer.py
@@ -3,12 +3,14 @@ from pathlib import Path
 from urllib.parse import urlparse, parse_qs
 import pandas as pd
 from youtube_transcript_api import YouTubeTranscriptApi
+from deepmultilingualpunctuation import PunctuationModel
 
 DATA_FOLDER = Path("data")
 MODEL_PATH = DATA_FOLDER / "sentence_importance_model.joblib"
 THRESHOLD_PATH = DATA_FOLDER / "sentence_importance_threshold.joblib"
 
 nlp = spacy.load("en_core_web_sm")
+model = PunctuationModel()
 ENTITY_TYPES = {"PERSON", "ORG", "GPE", "LOC"}
 
 def extract_video_id(video_url: str) -> str:
@@ -28,17 +30,25 @@ def extract_video_id(video_url: str) -> str:
 
 def get_transcript(video_url: str) -> str:
     video_id = extract_video_id(video_url)
-
     ytt_api = YouTubeTranscriptApi()
+
     fetched_transcript = ytt_api.fetch(video_id)
+    transcipt_text = ""
     if fetched_transcript:
         transcript_text = " ".join(snippet.text for snippet in fetched_transcript)
     else:
         raise ValueError("Missing transcript for the video")
 
-    segments = [snippet.text.strip() for snippet in fetched_transcript if snippet.text.strip()]
-    return segments
+    return transcript_text
+
+def add_punctuation(transcript_text):
+    result = model.restore_punctuation(transcript_text)
+    return result
 
 def compute_features(segments: list[str]) -> pd.DataFrame:
     pass
 
+
+# compute_features(get_transcript("https://www.youtube.com/watch?v=CMrHM8a3hqw"))
+# print(get_transcript("https://www.youtube.com/watch?v=CMrHM8a3hqw"))
+print(add_punctuation(get_transcript("https://www.youtube.com/watch?v=CMrHM8a3hqw")))
